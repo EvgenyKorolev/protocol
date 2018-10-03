841bba8d5e8ccaf8afe7f1997f91353c49292761
EvgenyKorolev <79297269463@yandex.ru>
2018-10-03 13:03:53 +0300

    Прикрутчен отдельно редактор показывающий только JavaScript Теперь можно прикрутить фичи: 1. Прикрутить настраиваемый сторонний редактор 2. Приучить визуальный редактор к работе с таблицами, хоть немного :)

---
+++ src/edit_plain_text.cpp
@@ -0,0 +1,6 @@
+#include "edit_plain_text.h"
+
+edit_plain_text::edit_plain_text()
+{
+
+}
---
+++ src/edit_plain_text.h
@@ -0,0 +1,12 @@
+#ifndef EDIT_PLAIN_TEXT_H
+#define EDIT_PLAIN_TEXT_H
+
+#include <QObject>
+
+class edit_plain_text : public QDialog
+{
+public:
+    edit_plain_text();
+};
+
+#endif // EDIT_PLAIN_TEXT_H
